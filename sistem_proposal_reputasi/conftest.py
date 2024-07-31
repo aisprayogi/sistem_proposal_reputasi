f536f30a9393061e2159abedcfd8ad65739796af
aisprayogi <aisprayogi@unhas.ac.id>
2024-07-31 10:31:06 +0800

    first commit

---
+++ sistem_proposal_reputasi/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sistem_proposal_reputasi.users.models import User
+from sistem_proposal_reputasi.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
